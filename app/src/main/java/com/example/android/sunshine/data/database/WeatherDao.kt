292fda14d33c00352ed612f1d0a537ea991e1dff
koto <jkotor@gmail.com>
2018-10-22 23:43:39 +0200

    Add Room dependencies and components

---
+++ app/src/main/java/com/example/android/sunshine/data/database/WeatherDao.kt
@@ -0,0 +1,16 @@
+package com.example.android.sunshine.data.database
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import java.util.*
+
+@Dao
+interface WeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun bulkInsert(vararg weather: WeatherEntry)
+
+    @Query("SELECT * FROM weather WHERE date = :date")
+    fun getWeatherByDate(date: Date): WeatherEntry
+}
